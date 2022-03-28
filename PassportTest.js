cf7a1959d2e0a51af4c8aef18b16e165adc13313
CardwellInc <thebusinesssuite@gmail.com>
2022-03-27 18:47:37 -0700

    Create PassportTest.js

---
+++ PassportTest.js
@@ -0,0 +1,39 @@
+const passport = require('passport'),
+      Strategy = require('passport-local').Strategy,
+      User = require('./models/user')
+
+// Local Strategy
+passport.use(new Strategy( (username, password, done) => {
+    User.findOne({username: username}, (err, user) => {
+      
+      // If any error
+      if (err) { return done(err) }
+  
+      // If no user found
+      if (!user) {
+        return done(null, false, {
+          message: 'No user found.'
+        })
+      }
+  
+      // Password not matched
+      if (user.password != password) {
+        return done(null, false, {
+          message: 'Password not matched.'
+        })
+      }
+  
+      return done(null, user)
+    })
+  }))
+
+// Session
+passport.serializeUser( (user, done) => done(null, user.id) )
+passport.deserializeUser( (id, done) => {
+  User.findById(id, (err, user) => {
+    if (err) { return done(err) }
+    done(null, user)
+  })
+})
+
+module.exports = passport
