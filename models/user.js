6d54bbbadad9e064a515f924078712fb042cce22
CardwellInc <thebusinesssuite@gmail.com>
2022-06-12 16:47:50 -0700

    Adding Re-Amortized
    Adjusted spelling on pages
    
    Added Re-Amortized page (still under construction)

--- models/user.js
+++ models/user.js
@@ -25,7 +25,7 @@ const userSchema = new mongoose.Schema({
 
 userSchema.pre('save', function(next) {
     if(this.isModified('password')){
-        bcrypt.hash(this.password, 10, (err, hash) => {
+        bcrypt.hash(this.password, 20, (err, hash) => {
             if(err) return next(err);
 
             this.password = hash;
